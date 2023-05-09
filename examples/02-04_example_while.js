4696172c611a42918cca550c4c21a4a58c992533
kevinelong <kevinelong@gmail.com>
2023-05-09 12:58:09 -0700

    example

---
+++ examples/02-04_example_while.js
@@ -0,0 +1,17 @@
+
+// let number = 1;
+// let iteration = 1; //increasing versions "mile post 0" a cycle
+// const LIMIT = 6
+
+// while (iteration < LIMIT) { //REPEAT IF(iteration is less than LIMIT)
+//     number = number * 2; // Double the number earch time we loop to grow
+//     console.log(iteration, number); //times cycled, how big we have grown
+//     iteration++; // iteration = iteration + 1
+// }//bottom of loop jups up to line 6 an re-evaluates the expression
+
+// console.log("FINISH because iteration is no longer less than LIMIT")
+
+for(i=0; i < 5; i++){ //START
+    console.log(i, "Piere");
+}
+console.log("DONE")
