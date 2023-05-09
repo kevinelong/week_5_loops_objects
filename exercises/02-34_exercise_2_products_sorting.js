9dac536f8f07719019794fe40bf457c85efd8349
kevinelong <kevinelong@gmail.com>
2023-05-09 10:53:19 -0700

    sorting

---
+++ exercises/02-34_exercise_2_products_sorting.js
@@ -0,0 +1,39 @@
+let products = [
+    {product: "Zaphods' Doubleheads", price: 5.79},
+    {product: "Gummy Worms", price: 5.79},
+    {product: "Plain M&Ms", price: 2.89},
+    {product: "Peanut M&Ms", price: 2.89},
+    {product: "Swedish Fish", price: 3.79},
+    {product: "Annies Stars", price: 3.79},
+    // TODO: fill the array with 10 candies of various
+    // price ranges
+];
+
+    function printProductList(products){
+        for(let i=0;i<products.length;i++){
+            console.log(products[i].product, products[i].price);
+        }
+    }
+
+    products.sort((a,b)=>{
+        if (a.product < b.product){
+            return -1;
+        }else if (a.product == b.product){
+             return 0;
+        }else{
+            return 1;
+        }
+    })
+
+    printProductList(products)
+console.log("-------------------")
+    products.sort((a,b)=>{
+        if (a.price < b.price){
+            return -1;
+        }else if (a.price == b.price){
+             return 0;
+        }else{
+            return 1;
+        }
+    })
+    printProductList(products)
